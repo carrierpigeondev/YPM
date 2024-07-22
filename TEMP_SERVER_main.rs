76e3d4ab9d9f8f33bb1482321284c006961916e7
Carrier Pigeon <132307322+carrierpigeondev@users.noreply.github.com>
2024-07-21 21:02:52 -0500

    Update TEMP_SERVER_main.rs

--- TEMP_SERVER_main.rs
+++ TEMP_SERVER_main.rs
@@ -1,9 +1,15 @@
 // don't have access to Cargo or any syntax analyzer; this is just a rough draft
 
 use axum::{
-  
+    Router,
+    routing::post,
+}
+
+async fn handle_package() {
+
 }
 
 async fn main() {
-  
+    let service = Router::new()
+        .route("/package", post(handle_package));
 }
